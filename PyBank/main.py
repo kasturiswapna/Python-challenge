10c411b3e6cbaac05cb54a2f5c824dc193c1963e
LAPTOP-T0V8VGO7\kastu <kasturiswapna2001@gmail.com>
2021-02-09 23:28:48 -0500

    Initial version

---
+++ PyBank/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 23:05:20 2021
+
+@author: kastu
+"""
+
---
+++ PyPoll/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 23:07:34 2021
+
+@author: kastu
+"""
+
